d5990fe98d5faa4cdbfbe51c95d365ce59b7cf8f
Cleiston <cleistonrs@gmail.com>
2020-03-25 15:16:44 -0300

    Create new non-governmental organization's page

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 import './styles.css';
 import logoImg from '../../assets/logo.svg';
@@ -16,10 +17,10 @@ export default function Logon () {
                     <input placeholder="Sua ID" />
                     <button className="button" type="submit">Entrar</button>
 
-                    <a href="/register">
+                    <Link to="/register" className="back-link">
                         <FiLogIn />
                         Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
 
